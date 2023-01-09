59982235b51e5071a95478af0f82ae87007e88bc
Marc Anthony Card <marcacard@gmail.com>
2023-01-08 21:49:00 -0500

    update: og:description

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
         <meta property="og:type" content="website"/>
         <meta property="og:site_name" content="CronSchedule.xyz"/>
         <meta property="og:title" content="Cron Schedules Simplified"/>
-        <meta property="og:description" content="Easily create cron schedules with CronSchedule.xyz. Our user-friendly platform generates cron expressions in plain English, making it simple to schedule tasks and scripts on your server."/>
+        <meta property="og:description" content="Effortlessly create cron expressions with CronSchedule.xyz, powered by AI. Simply describe your schedule in plain English and let our AI do the rest."/>
         <meta property="og:url" content="https://cronschedule.xyz"/>
         <meta property="og:image" content="https://cronschedule.xyz/cronschedule-og-web-image.png"/>
         <meta name="twitter:card" content="summary_large_image"></meta>
